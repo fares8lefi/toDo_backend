de138c441cff04d9bb9acc0f5ef682fe7e646963
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-09-13 17:41:17 +0200

    create task schema

---
+++ models/taskSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const taskSchema= new mongoose.Schema(
+    {
+        title:{
+            type: String,
+            required: true
+        },
+        description:{
+            type :String,
+            default:""
+        },
+        completed:{
+            type:Boolean,
+            default:false
+        },
+         user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    }
+    }
+    );
+
+    const Task = mongoose.model("Task",taskSchema);
+    module.exports = Task;
